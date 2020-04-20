07b6c6b3fef9f5d5d2025d247e22c5ed56545632
Rong Zhao Lee <rongzhao195@gmail.com>
2020-04-20 22:57:46 +0800

    tune performance Audit

--- server.js
+++ server.js
@@ -5,6 +5,20 @@ const compression = require('compression');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(compression());
+
+
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+};
+
 app.use(favicon(__dirname + "/dist/favicon.ico"));
 
 // the __dirname is the current directory from where the script is running
